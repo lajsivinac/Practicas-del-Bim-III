c29a0b13720cb03ad113a18f472fdbf49b7cd0cf
lajsivinac <166849801+lajsivinac@users.noreply.github.com>
2024-07-12 10:33:33 -0600

    Update Motor.ino

--- P1- Parte2 Bim III/Motor.ino
+++ P1- Parte2 Bim III/Motor.ino
@@ -1,7 +1,6 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-// Directivas del preprocesador
 #define SENSOR_HALL_PIN 2
 #define SENSOR_IR_PIN 3
 #define POT_PIN A0
@@ -10,13 +9,13 @@
 #define MOTOR_PIN3 10
 #define MOTOR_PIN4 11
 
-// LCD
+
 #define LCD_ADDR 0x27
 #define LCD_ROWS 2
 #define LCD_COLS 16
 LiquidCrystal_I2C pantalla(LCD_ADDR, LCD_COLS, LCD_ROWS);
 
-// Variables
+
 unsigned int lecturaPot;
 unsigned long cuentaRevoluciones = 0;
 unsigned long tiempoPrevio = 0;
@@ -24,7 +23,6 @@ bool estadoHall = false;
 bool estadoIR = false;
 volatile bool bandera = false;
 
-// Secuencia de pasos para el motor paso a paso
 int pasosMotor[8][4] = {
   {1, 0, 0, 0},
   {1, 1, 0, 0},
@@ -36,22 +34,20 @@ int pasosMotor[8][4] = {
   {1, 0, 0, 1}
 };
 
-// Función para leer el valor del ADC
 unsigned int leerADC(int pin) {
   return analogRead(pin);
 }
 
-// Interrupción para el sensor de efecto Hall
 void interrupcionHall() {
   cuentaRevoluciones++;
   bandera = true;
 }
 
 void setup() {
-  // Inicializar comunicación serial
+  
   Serial.begin(9600);
 
-  // Configuración de pines
+ 
   pinMode(SENSOR_HALL_PIN, INPUT);
   pinMode(SENSOR_IR_PIN, INPUT);
   pinMode(MOTOR_PIN1, OUTPUT);
@@ -59,29 +55,28 @@ void setup() {
   pinMode(MOTOR_PIN3, OUTPUT);
   pinMode(MOTOR_PIN4, OUTPUT);
 
-  // Configuración de la interrupción
   attachInterrupt(digitalPinToInterrupt(SENSOR_HALL_PIN), interrupcionHall, FALLING);
 
-  // Inicialización de la pantalla LCD
+ 
   pantalla.init();
   pantalla.backlight();
   pantalla.setCursor(0, 0);
   pantalla.print("Contador de rev.");
 
-  // Variables de tiempo
+
   tiempoPrevio = millis();
 }
 
 void loop() {
-  // Leer estado del sensor de obstáculos
+  
   estadoIR = digitalRead(SENSOR_IR_PIN);
 
   if (estadoIR == LOW) {
-    // Leer valor del potenciómetro y mapearlo
+    
     lecturaPot = leerADC(POT_PIN);
     int velocidadMotor = map(lecturaPot, 0, 1023, 1, 3);
 
-    // Controlar el motor paso a paso
+    
     for (int i = 0; i < 8; i++) {
       digitalWrite(MOTOR_PIN1, pasosMotor[i][0]);
       digitalWrite(MOTOR_PIN2, pasosMotor[i][1]);
@@ -90,17 +85,15 @@ void loop() {
       delay(velocidadMotor);
     }
 
-    // Medir y mostrar las revoluciones
     if (millis() - tiempoPrevio >= 1000) {
       tiempoPrevio = millis();
       pantalla.setCursor(0, 1);
       pantalla.print("No. De Rev: ");
       pantalla.print(cuentaRevoluciones);
       pantalla.print("    ");
-      cuentaRevoluciones = 0; // Resetear contador cada segundo
+      cuentaRevoluciones = 0; 
     }
   } else {
-    // Si el sensor de obstáculos no detecta nada, parar el motor y borrar la pantalla
     digitalWrite(MOTOR_PIN1, LOW);
     digitalWrite(MOTOR_PIN2, LOW);
     digitalWrite(MOTOR_PIN3, LOW);
